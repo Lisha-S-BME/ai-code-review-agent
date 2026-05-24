d81bb8504ec7b9fdbff792635870a98aa7d61239
LISHA S <153447247+Lisha-S-BME@users.noreply.github.com>
2026-05-24 09:54:28 +0530

    Add sample.js code with issues

---
+++ sample.js
@@ -0,0 +1,27 @@
+const API_KEY = "sk-1234567890abcdef";
+
+function getUserData(userId) {
+  var x = fetchUser(userId);
+  return x.name.toUpperCase();
+}
+
+function fetchUser(userId) {
+  if (!userId) {
+    return null;
+  }
+  return { id: userId, name: "Test User" };
+}
+
+function processItems(items) {
+  for (let i = 0; i < items.length; i++) {
+    for (let j = 0; j < items.length; j++) {
+      console.log(items[i], items[j]);
+    }
+  }
+}
+
+function calculateDiscount(price) {
+  return price - 10;
+}
+
+getUserData(null);
